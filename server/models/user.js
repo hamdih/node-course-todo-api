9ecf57d6f11d91a22b768c01f341e6a7ff83be08
hamdih <hamdy2224@gmail.com>
2019-02-08 19:00:57 -0600

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -55,6 +55,15 @@ UserSchema.methods.generateAuthToken = function() {
 	});
 };
 
+UserSchema.methods.removeToken = function(token) {
+	var user = this;
+	return user.update({
+		$pull: {
+			tokens: {token}
+		}
+	})
+
+};
 UserSchema.statics.findByToken = function(token) {	//model method is static
 	var User = this; //calling the model
 	var decoded;
